aaac2f2e7cf4064d76cad2900008037a22c03f74
Pamagea <paul.pesak@gmx.at>
2023-05-12 12:14:14 +0200

    First commit

---
+++ app/src/main/java/com/example/m3_01_08_reiseplaner/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.m3_01_08_reiseplaner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //If you see this, your pull was successful
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
