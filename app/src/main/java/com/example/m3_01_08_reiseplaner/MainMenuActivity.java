df8b63356a9836cee7dff4dc8dd1731705064b75
Pamagea <paul.pesak@gmx.at>
2023-05-17 14:12:07 +0200

    Added base layout for the Main Menu Activity without RecyclerView and Search Travel Button

---
+++ app/src/main/java/com/example/m3_01_08_reiseplaner/MainMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.m3_01_08_reiseplaner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
