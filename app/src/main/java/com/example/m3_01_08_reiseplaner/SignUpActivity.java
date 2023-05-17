48d8e951e5d1fec018982003863797def80acac0
Yernaz Togizbayev <yernaz.togizbayev@gmail.com>
2023-05-17 11:11:39 +0200

    create Sign Up functionality

--- app/src/main/java/com/example/m3_01_08_reiseplaner/SignUpActivity.java
+++ app/src/main/java/com/example/m3_01_08_reiseplaner/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.m3_01_08_reiseplaner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void SignUpButtonPress(View view) {
+        Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+        startActivity(intent);
     }
 }
