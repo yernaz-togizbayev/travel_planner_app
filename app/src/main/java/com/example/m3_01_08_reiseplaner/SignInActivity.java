0ff71a24d50cbc40974974a3fb8ff9566d2be4e5
yernaz-togizbayev <yernaz.togizbayev@gmail.com>
2023-05-18 15:46:36 +0200

    handle SignUp button to perform actuall SignIn

--- app/src/main/java/com/example/m3_01_08_reiseplaner/SignInActivity.java
+++ app/src/main/java/com/example/m3_01_08_reiseplaner/SignInActivity.java
@@ -1,10 +1,15 @@
 package com.example.m3_01_08_reiseplaner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -20,7 +25,30 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void SignInButtonPress(View view) {
-        Intent intent = new Intent(SignInActivity.this, MainMenuActivity.class);
-        startActivity(intent);
+        EditText emailEditText = findViewById(R.id.SigninEmailEditText);
+        EditText passwordEditText = findViewById(R.id.SigninPasswordEditText);
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        User user = findUserByEmail(email);
+
+        if (user != null && user.getPassword().equals(password)) {
+            Toast.makeText(this, "Sign in successful", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(SignInActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "Invalid email or password", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Nullable
+    private User findUserByEmail(String email) {
+        for (User user : SignUpActivity.getUserList()) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
     }
 }
