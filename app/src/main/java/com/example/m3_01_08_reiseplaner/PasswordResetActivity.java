2f7becab968e42a6fc7c444ae2ca97364314a058
Yernaz Togizbayev <yernaz.togizbayev@gmail.com>
2023-05-19 23:00:30 +0200

    create PasswordResetActivity and screen

---
+++ app/src/main/java/com/example/m3_01_08_reiseplaner/PasswordResetActivity.java
@@ -0,0 +1,19 @@
+package com.example.m3_01_08_reiseplaner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+public class PasswordResetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reset);
+    }
+
+}
