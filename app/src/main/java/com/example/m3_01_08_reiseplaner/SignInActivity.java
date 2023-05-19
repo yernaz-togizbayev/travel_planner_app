a0ff88604c4f082a13e67f6742e0f7c41d803bb6
yernaz-togizbayev <yernaz.togizbayev@gmail.com>
2023-05-19 21:46:05 +0200

    add documentation for SignInActivity

--- app/src/main/java/com/example/m3_01_08_reiseplaner/SignInActivity.java
+++ app/src/main/java/com/example/m3_01_08_reiseplaner/SignInActivity.java
@@ -11,19 +11,40 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+/**
+ * The class which represents Sign In screen. It has a functionality to sign in to EasyTravel
+ * application, where users will be asked to enter their email address and password, and then
+ * navigate to the main menu screen if the sign in succeeded.
+ */
 public class SignInActivity extends AppCompatActivity {
 
+    /**
+     * Initialization of SignInActivity and setting up the view of a content
+     * @param savedInstanceState State of saved instance.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
     }
 
+    /**
+     * Handles the button press to go back from the sign in screen to the start screen.
+     * @param view The button that was pressed by the user.
+     */
     public void SignInGoBackPress(View view) {
         Intent intent = new Intent(SignInActivity.this, StartScreenActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * This method handles the button press event for the sign-in button. It performs validation
+     * check whether email address and password are match. In case email could not be found in the
+     * list of users or found, but password is incorrect, popup message will appear to inform user
+     * that email or password are incorrect. If user email and password entered correctly, popup
+     * message will inform user that he successfully logged in and will be switched to main menu.
+     * @param view The button that was pressed by the user.
+     */
     public void SignInButtonPress(View view) {
         EditText emailEditText = findViewById(R.id.SigninEmailEditText);
         EditText passwordEditText = findViewById(R.id.SigninPasswordEditText);
@@ -42,6 +63,11 @@ public class SignInActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Finds a user with the specified email in the user list.
+     * @param email The email to search for.
+     * @return User object if found in list of users, null otherwise.
+     */
     @Nullable
     private User findUserByEmail(String email) {
         for (User user : SignUpActivity.getUserList()) {
@@ -52,6 +78,11 @@ public class SignInActivity extends AppCompatActivity {
         return null;
     }
 
+    /**
+     * Function to display a popup message with text and background color.
+     * @param textMessage The text to display in the popup message.
+     * @param backgroundColor The background color of the popup message.
+     */
     private void showSignInPopupMessage(String textMessage, int backgroundColor) {
         Toast popupMessage = Toast.makeText(this, textMessage, Toast.LENGTH_LONG);
 
