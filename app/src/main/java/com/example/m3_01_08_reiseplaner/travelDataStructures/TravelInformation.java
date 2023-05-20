64b98deb6f4df7512377d9382380cc7d48e0e5a6
Pamagea <paul.pesak@gmx.at>
2023-05-20 19:00:19 +0200

    Adding Travel Information Datastructure

---
+++ app/src/main/java/com/example/m3_01_08_reiseplaner/travelDataStructures/TravelInformation.java
@@ -0,0 +1,53 @@
+package com.example.m3_01_08_reiseplaner.travelDataStructures;
+
+import com.example.m3_01_08_reiseplaner.enums.ETravelPreference;
+
+import java.time.LocalDate;
+
+public class TravelInformation {
+    private String startCity;
+    private String startCountry;
+    private String destinationCity;
+    private String destinationCountry;
+    private LocalDate startDate;
+    private LocalDate backTravelDate;
+    private ETravelPreference preference;
+
+    public TravelInformation(String startCity, String startCountry, String destinationCity, String destinationCountry, LocalDate startDate, LocalDate backTravelDate, ETravelPreference preference) {
+        this.startCity = startCity;
+        this.startCountry = startCountry;
+        this.destinationCity = destinationCity;
+        this.destinationCountry = destinationCountry;
+        this.startDate = startDate;
+        this.backTravelDate = backTravelDate;
+        this.preference = preference;
+    }
+
+    public String getStartCity() {
+        return startCity;
+    }
+
+    public String getStartCountry() {
+        return startCountry;
+    }
+
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    public String getDestinationCountry() {
+        return destinationCountry;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getBackTravelDate() {
+        return backTravelDate;
+    }
+
+    public ETravelPreference getPreference() {
+        return preference;
+    }
+}
